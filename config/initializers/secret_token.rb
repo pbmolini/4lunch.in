5d0c8e1970d3044994ed77c485c005314ddb89d8
pbmolini <pbmolini@fbk.eu>
2013-12-07 13:52:07 +0100

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ForLunchIn::Application.config.secret_key_base = '7d6ac4365f5d77fb7333e8e482de0a640e5d5a768b2891b644c9ca0ad27adb30a6dfb8aae400f040bf75fefe2d171c5413be9cce5e516f53dc76b93ec41eabe5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ForLunchIn::Application.config.session_store :cookie_store, key: '_for_lunch_in_session'
