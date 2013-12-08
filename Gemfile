da9c123b8bee7076fa9553cbcde5e553717dfd9b
Aaron <aaron.ciaghi@gmail.com>
2013-12-08 10:36:13 +0100

    added datepicker

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # gem 'therubyracer', platforms: :ruby
 
 gem "bootstrap-generators", '~> 3.0'
+gem 'bootstrap-datepicker-rails'
 
 gem "font-awesome-rails"
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def date_format_js format=nil
+    format ||= t('date.formats.default')
+    format.gsub('%Y','yyyy').gsub('%m','mm').gsub('%d','dd')
+  end
+
 end
