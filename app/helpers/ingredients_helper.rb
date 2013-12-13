b97967e63b953a83d9fe2031165b933499850d28
Aaron <aaron.ciaghi@gmail.com>
2013-12-13 15:12:14 +0100

    generalized functions

--- app/helpers/ingredients_helper.rb
+++ app/helpers/ingredients_helper.rb
@@ -2,8 +2,8 @@ module IngredientsHelper
 
 	def ingredient_labels dish
 		capture do
-			Ingredient.columns.collect {|c| c.name if c.type == :boolean}.compact.each do |att|
-				concat(content_tag(:span, att, class: 'label label-info')) if dish.ingredients.map {|i| i.send("#{att}?") }.reduce(:&)
+			Ingredient.flags.each do |att|
+				concat(content_tag(:span, att, class: 'label label-info')) if dish.send("is_#{att}?")
 				concat ' '
 			end
 		end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,10 +1,16 @@
 class Dish < ActiveRecord::Base
 
-	belongs_to :canteen
-	belongs_to :menu
-	has_and_belongs_to_many :ingredients
+  belongs_to :canteen
+  belongs_to :menu
+  has_and_belongs_to_many :ingredients
 
-	TYPES = [n_("Hors d'oeuvre","Hors d'oeuvre",1),n_('Entrée','Entrées',1),n_('Second Course','Second Courses',1),n_('Main','Main',1),n_('Side','Side',1),n_('Dessert','Desserts',1),n_('Beverage','Beverages',1)].freeze
-	#TYPES = %w{_('Antipasto'),_('Primo'),_('Secondo'),_('Piatto Unico'),_('Contorno'),_('Dessert'),_('Bevanda')}
+  TYPES = [n_("Hors d'oeuvre","Hors d'oeuvre",1),n_('Entrée','Entrées',1),n_('Second Course','Second Courses',1),n_('Main','Main',1),n_('Side','Side',1),n_('Dessert','Desserts',1),n_('Beverage','Beverages',1)].freeze
+  #TYPES = %w{_('Antipasto'),_('Primo'),_('Secondo'),_('Piatto Unico'),_('Contorno'),_('Dessert'),_('Bevanda')}
 
+
+  Ingredient.flags.each do |att|
+    define_method "is_#{att}?" do
+      self.ingredients.map {|i| i.send("#{att}?") }.reduce(Ingredient.reduce_operator_for att)
+    end
+  end
 end
